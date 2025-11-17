70a84dd89a849e2b1a2f8a43b5c2210a2565652c
arFaisal043 <arfaisal463@gmail.com>
2025-11-17 21:30:06 +0600

    second commit

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 
-    vector<int> arr = {2, 1, 4, 6, 78, 1, 124, 26, 47, 4};
+    vector<int> arr = {2, 1, 4, 6, 78, 1, 124, 26, 47, 4, 2};
     int n = arr.size();
 
     for(int i = 0; i < n-1; i++) {
